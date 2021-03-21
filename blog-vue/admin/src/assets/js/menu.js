9c044523d9a7ceb4b052fbcef4c8afe27e6eddcb
xiaojie <1192176811@qq.com>
2021-03-21 12:04:32 +0800

    fix

---
+++ blog-vue/admin/src/assets/js/menu.js
@@ -0,0 +1,44 @@
+import Layout from "@/layout/index.vue";
+import router from "../../router";
+import store from "../../store";
+import axios from "axios";
+import Vue from "vue";
+
+export function generaMenu() {
+  // 查询用户菜单
+  axios.get("/api/admin/user/menus").then(({ data }) => {
+    if (data.flag) {
+      var userMenuList = data.data;
+      userMenuList.forEach(item => {
+        if (item.icon != null) {
+          item.icon = "iconfont " + item.icon;
+        }
+        if (item.component == "Layout") {
+          item.component = Layout;
+        }
+        if (item.children && item.children.length > 0) {
+          item.children.forEach(route => {
+            route.icon = "iconfont " + route.icon;
+            route.component = loadView(route.component);
+          });
+        }
+      });
+      // 添加侧边栏菜单
+      store.commit("saveUserMenuList", userMenuList);
+      // 添加菜单到路由
+      router.addRoutes(userMenuList);
+      if (userMenuList.length == 0) {
+        Vue.prototype.$message.error("没有访问权限");
+        router.push({ path: "/login" });
+      } else {
+        Vue.prototype.$message.success("登录成功");
+        router.push({ path: "/" });
+      }
+    }
+  });
+}
+
+export const loadView = view => {
+  // 路由懒加载
+  return resolve => require([`@/views${view}`], resolve);
+};
