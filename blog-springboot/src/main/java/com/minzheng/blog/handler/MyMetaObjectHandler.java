60447ec3983c53385f5b99255e9f5d1a0338c07d
yezhiqiu <1192176811@qq.com>
2021-08-12 21:11:48 +0800

    修改时区BUG

--- blog-springboot/src/main/java/com/minzheng/blog/handler/MyMetaObjectHandler.java
+++ blog-springboot/src/main/java/com/minzheng/blog/handler/MyMetaObjectHandler.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 /**
  * mybatis plus自动填充
@@ -20,14 +21,13 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
         log.info("start insert fill ....");
-        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
-
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now(ZoneId.of("Asia/Shanghai")));
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
         log.info("start update fill ....");
-        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now(ZoneId.of("Asia/Shanghai")));
     }
 
 }
