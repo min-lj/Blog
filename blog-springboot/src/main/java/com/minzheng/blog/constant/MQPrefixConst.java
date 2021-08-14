529df517c1b89ee6e5765df692d4aa14cc64a6a3
yezhiqiu <1192176811@qq.com>
2021-08-14 21:26:38 +0800

    修改maxwell配置

--- blog-springboot/src/main/java/com/minzheng/blog/constant/MQPrefixConst.java
+++ blog-springboot/src/main/java/com/minzheng/blog/constant/MQPrefixConst.java
@@ -12,21 +12,21 @@ public class MQPrefixConst {
     /**
      * maxwell交换机
      */
-    public static final String MAXWELL_EXCHANGE = "maxwell";
+    public static final String MAXWELL_EXCHANGE = "maxwell_exchange";
 
     /**
      * maxwell队列
      */
-    public static final String MAXWELL_QUEUE = "article";
+    public static final String MAXWELL_QUEUE = "maxwell_queue";
 
     /**
      * email交换机
      */
-    public static final String EMAIL_EXCHANGE = "send";
+    public static final String EMAIL_EXCHANGE = "email_exchange";
 
     /**
      * 邮件队列
      */
-    public static final String EMAIL_QUEUE = "email";
+    public static final String EMAIL_QUEUE = "email_queue";
 
 }
