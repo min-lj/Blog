3c343a12c19c3ca1c93514baa40c8213a1458ff3
yezhiqiu <1192176811@qq.com>
2021-09-21 23:30:07 +0800

    优化第三方登录逻辑

--- blog-springboot/src/main/java/com/minzheng/blog/strategy/impl/AbstractSocialLoginStrategyImpl.java
+++ blog-springboot/src/main/java/com/minzheng/blog/strategy/impl/AbstractSocialLoginStrategyImpl.java
@@ -62,36 +62,11 @@ public abstract class AbstractSocialLoginStrategyImpl implements SocialLoginStra
         // 判断是否注册
         UserAuth user = getUserAuth(socialToken);
         if (Objects.nonNull(user)) {
-            // 返回数据库信息
+            // 返回数据库用户信息
             userDetailDTO = getUserDetail(user, ipAddress, ipSource);
         } else {
-            // 获取第三方用户信息
-            SocialUserInfoDTO socialUserInfo = getSocialUserInfo(socialToken);
-            // 保存用户信息
-            UserInfo userInfo = UserInfo.builder()
-                    .nickname(socialUserInfo.getNickname())
-                    .avatar(socialUserInfo.getAvatar())
-                    .build();
-            userInfoDao.insert(userInfo);
-            // 保存账号信息
-            UserAuth userAuth = UserAuth.builder()
-                    .userInfoId(userInfo.getId())
-                    .username(socialToken.getOpenId())
-                    .password(socialToken.getAccessToken())
-                    .loginType(socialToken.getLoginType())
-                    .lastLoginTime(LocalDateTime.now(ZoneId.of(SHANGHAI.getZone())))
-                    .ipAddress(ipAddress)
-                    .ipSource(ipSource)
-                    .build();
-            userAuthDao.insert(userAuth);
-            // 绑定角色
-            UserRole userRole = UserRole.builder()
-                    .userId(userInfo.getId())
-                    .roleId(RoleEnum.USER.getRoleId())
-                    .build();
-            userRoleDao.insert(userRole);
-            // 封装登录信息
-            userDetailDTO = userDetailsService.convertUserDetail(userAuth, request);
+            // 返回新增用户信息
+            userDetailDTO = saveUserDetail(socialToken, ipAddress, ipSource);
         }
         // 将登录信息放入springSecurity管理
         UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetailDTO, null, userDetailDTO.getAuthorities());
@@ -117,6 +92,17 @@ public abstract class AbstractSocialLoginStrategyImpl implements SocialLoginStra
     public abstract SocialUserInfoDTO getSocialUserInfo(SocialTokenDTO socialTokenDTO);
 
     /**
+     * 获取用户账号
+     *
+     * @return {@link UserAuth} 用户账号
+     */
+    private UserAuth getUserAuth(SocialTokenDTO socialTokenDTO) {
+        return userAuthDao.selectOne(new LambdaQueryWrapper<UserAuth>()
+                .eq(UserAuth::getUsername, socialTokenDTO.getOpenId())
+                .eq(UserAuth::getLoginType, socialTokenDTO.getLoginType()));
+    }
+
+    /**
      * 获取用户信息
      *
      * @param user      用户账号
@@ -136,14 +122,43 @@ public abstract class AbstractSocialLoginStrategyImpl implements SocialLoginStra
     }
 
     /**
-     * 获取用户账号
+     * 新增用户信息
      *
-     * @return {@link UserAuth} 用户账号
+     * @param socialToken token信息
+     * @param ipAddress   ip地址
+     * @param ipSource    ip源
+     * @return {@link UserDetailDTO} 用户信息
      */
-    private UserAuth getUserAuth(SocialTokenDTO socialTokenDTO) {
-        return userAuthDao.selectOne(new LambdaQueryWrapper<UserAuth>()
-                .eq(UserAuth::getUsername, socialTokenDTO.getOpenId())
-                .eq(UserAuth::getLoginType, socialTokenDTO.getLoginType()));
+    private UserDetailDTO saveUserDetail(SocialTokenDTO socialToken, String ipAddress, String ipSource) {
+        UserDetailDTO userDetailDTO;
+        // 获取第三方用户信息
+        SocialUserInfoDTO socialUserInfo = getSocialUserInfo(socialToken);
+        // 保存用户信息
+        UserInfo userInfo = UserInfo.builder()
+                .nickname(socialUserInfo.getNickname())
+                .avatar(socialUserInfo.getAvatar())
+                .build();
+        userInfoDao.insert(userInfo);
+        // 保存账号信息
+        UserAuth userAuth = UserAuth.builder()
+                .userInfoId(userInfo.getId())
+                .username(socialToken.getOpenId())
+                .password(socialToken.getAccessToken())
+                .loginType(socialToken.getLoginType())
+                .lastLoginTime(LocalDateTime.now(ZoneId.of(SHANGHAI.getZone())))
+                .ipAddress(ipAddress)
+                .ipSource(ipSource)
+                .build();
+        userAuthDao.insert(userAuth);
+        // 绑定角色
+        UserRole userRole = UserRole.builder()
+                .userId(userInfo.getId())
+                .roleId(RoleEnum.USER.getRoleId())
+                .build();
+        userRoleDao.insert(userRole);
+        // 封装登录信息
+        userDetailDTO = userDetailsService.convertUserDetail(userAuth, request);
+        return userDetailDTO;
     }
 
 }
