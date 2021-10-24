23f88df343b5e7b1665bd34ac5e46ace9d772ece
yezhiqiu <1192176811@qq.com>
2021-10-24 14:23:52 +0800

    修改音乐接口

--- blog-vue/blog/src/components/zw-player/api/music.js
+++ blog-vue/blog/src/components/zw-player/api/music.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 //获取歌词
 export const getWords = id => {
-  return axios.get(`https://autumnfish.cn/lyric?id=${id}`);
+  return axios.get(`https://n.xlz122.cn/api/lyric?id=${id}`);
 };
 //获取歌曲详情
 export const getMusicInfo = id => {
-  return axios.get(`https://autumnfish.cn/song/detail?ids=${id}`);
+  return axios.get(`https://n.xlz122.cn/api/song/detail?ids=${id}`);
 };
 //获取歌曲url
 export const getMusicUrl = id => {
-  return axios.get(`https://autumnfish.cn/song/url?id=${id}`);
+  return axios.get(`https://n.xlz122.cn/api/song/url?id=${id}`);
 };
 //获取热门歌曲
 export const getHotMusic = id => {
-  return axios.get(`https://autumnfish.cn/playlist/detail?id=${id}`);
+  return axios.get(`https://n.xlz122.cn/api/playlist/detail?id=${id}`);
 };
 //获取搜索建议
 export const getSearchSuggest = key => {
-  return axios.get(`https://autumnfish.cn/search/suggest?keywords=${key}`);
+  return axios.get(`https://n.xlz122.cn/api/search/suggest?keywords=${key}`);
 };
 //获取歌曲热门评论
 export const getHotTalk = id => {
-  return axios.get(`https://autumnfish.cn/comment/music?id=${id}&limit=3`);
+  return axios.get(`https://n.xlz122.cn/api/comment/music?id=${id}&limit=3`);
 };
